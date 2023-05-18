dcf3bef2ae92399b54aff3f314258c6453467bc8
ROBERT KIRBY <2021631@chester.ac.uk>
2023-05-18 15:10:21 +0100

    Robert changes

--- WellnessSite/Pages/Index.cshtml.cs
+++ WellnessSite/Pages/Index.cshtml.cs
@@ -15,7 +15,9 @@ namespace WellnessSite.Pages
         private readonly SignInManager<ApplicationUser> _sim;
         private readonly WellnessSiteContext _context;
         public Preferences p;
-
+        public IList<Service> Services { get; set; } = default!;
+        public IList<Bookmarks> bookmarks;
+        public string? name;
         public IndexModel(SignInManager<ApplicationUser> sim, UserManager<ApplicationUser> um, WellnessSiteContext con)
         {
             _sim = sim;
@@ -26,6 +28,18 @@ namespace WellnessSite.Pages
         public async Task OnGetAsync()
         {
             p = await UsefulFunctions.GetPreferences(_context, _um, _sim, User, this);
+            if (_sim.IsSignedIn(User))
+            {
+                ApplicationUser u = await _um.GetUserAsync(User);
+                if (u != null && u.Name != null)
+                {
+                    name = u.Name;
+                }
+
+                bookmarks = await _context.Bookmarks.Where(b => b.UserID == u.Id).ToListAsync();
+
+                Services = await _context.Service.ToListAsync();
+            }
         }
 
     }
